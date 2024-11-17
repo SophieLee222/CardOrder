8eabd3c4c47c331fcf37deabba7c78d61491c459
sofia va <sophia.lily.lee@gmail.com>
2024-11-17 22:17:03 +0300

    Form Tests added

---
+++ src/test/java/FormTest.java
@@ -0,0 +1,62 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FormTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+//        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldSendFormWithCorrectValues() {
+        WebElement form = driver.findElement(By.cssSelector("form"));
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Григорий");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79604578591");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
+    }
+
+    @Test
+    void shouldGiveErrorIfInorrectValues() {
+        WebElement form = driver.findElement(By.cssSelector("form"));
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Grisha");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+796048591");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String text = driver.findElement(By.cssSelector(".input__sub")).getText().trim();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text);
+    }
+
+}
