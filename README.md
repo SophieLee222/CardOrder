3b87e5431c8cc042836c80e890e1258034a25d61
sofia va <sophia.lily.lee@gmail.com>
2024-11-19 20:56:17 +0300

    New tests added

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/wra7ggslnb4a7tob?svg=true)](https://ci.appveyor.com/project/SophieLee222/cardorder)
--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -8,9 +8,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.Color;
 
+import javax.swing.*;
 import java.util.List;
 
+import static java.awt.SystemColor.text;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FormTest {
@@ -26,7 +29,7 @@ public class FormTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
-//        options.addArguments("--headless");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
     }
@@ -48,15 +51,63 @@ public class FormTest {
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
     }
 
+    //невалидное значение в поле имени и фамилии
     @Test
-    void shouldGiveErrorIfInorrectValues() {
+    void shouldGiveErrorIfIncorrectValueInNameField() {
         WebElement form = driver.findElement(By.cssSelector("form"));
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Grisha");
-        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+796048591");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79604578591");
         driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
         driver.findElement(By.cssSelector("button")).click();
-        String text = driver.findElement(By.cssSelector(".input__sub")).getText().trim();
+        String text = driver.findElement(By.cssSelector(".input_theme_alfa-on-white.input_invalid .input__sub")).getText().trim();
         assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text);
     }
 
+    //невалидное значение в поле телефона
+    @Test
+    void shouldGiveErrorIfIncorrectValueInPhoneField() {
+        WebElement form = driver.findElement(By.cssSelector("form"));
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Григорий");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("78591");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String text = driver.findElement(By.cssSelector(".input_theme_alfa-on-white.input_invalid .input__sub")).getText().trim();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text);
+    }
+
+    //пустое поле имени и фамилии
+    @Test
+    void shouldGiveErrorIfEmptyValueInNameField() {
+        WebElement form = driver.findElement(By.cssSelector("form"));
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79604578591");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String text = driver.findElement(By.cssSelector(".input_theme_alfa-on-white.input_invalid .input__sub")).getText().trim();
+        assertEquals("Поле обязательно для заполнения", text);
+    }
+
+    //пустое поле телефона
+    @Test
+    void shouldGiveErrorIfEmptyValueInPhoneField() {
+        WebElement form = driver.findElement(By.cssSelector("form"));
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Гриша");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String text = driver.findElement(By.cssSelector(".input_theme_alfa-on-white.input_invalid .input__sub")).getText().trim();
+        assertEquals("Поле обязательно для заполнения", text);
+    }
+
+    //неотмеченный чекбокс
+    @Test
+    void shouldGiveErrorIfCheckboxNotChecked() {
+        WebElement form = driver.findElement(By.cssSelector("form"));
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Гриша");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79604578591");
+        driver.findElement(By.cssSelector("button")).click();
+        String rgbaColor = driver.findElement(By.cssSelector(".checkbox.input_invalid")).getCssValue("color");
+        String hexColor = Color.fromString(rgbaColor).asHex();
+        assertEquals("#ff5c5c", hexColor);
+    }
 }
